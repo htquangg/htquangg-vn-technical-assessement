f22bb8b8c49946fdaeff78548b0662ad1dc01822
Quang HUYNH <htquangg@gmail.com>
2022-11-04 23:57:56 +0700

    feat: setup document **swagger**

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,20 +3,35 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  setupSwagger(app);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
-}
+})();
 
-bootstrap();
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('htquangg-vn-technical-assessement')
+    .setVersion('0.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('docs', app, document);
+}
